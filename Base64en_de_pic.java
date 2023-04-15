28c4384a52dc1073a8957c775f920cd2c96eba40
Enis Hoxha <enishoxha10@icloud.com>
2023-04-15 13:06:54 +0200

    Create Base64en_de_pic.java

---
+++ Base64en_de_pic.java
@@ -0,0 +1,12 @@
+import java.util.Base64;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Picture {
+    public static void main(String[] args) throws Exception {
+        // Load the image into the bytes array
+        byte[] imageBytes = Files.readAllBytes(Paths.get("C:\\Users\\ccc\\Pictures\\Saved Pictures\\Solid_black.jpg"));
+      
+      
+    }
+}
