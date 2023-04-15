5b9e474cf7cfba6111bc0588422936541e1bfac9
endritgjokaj <70038985+endritgjokaj@users.noreply.github.com>
2023-04-15 13:10:44 +0200

    Update Base64en_de_pic.java

--- Base64en_de_pic.java
+++ Base64en_de_pic.java
@@ -6,6 +6,10 @@ public class Picture {
     public static void main(String[] args) throws Exception {
         // Load the image into the bytes array
         byte[] imageBytes = Files.readAllBytes(Paths.get("C:\\Users\\ccc\\Pictures\\Saved Pictures\\Solid_black.jpg"));
+        
+         // Encode the string of bytes
+        String base64EncodedImage = Base64.getEncoder().encodeToString(imageBytes);
+        System.out.println("Encoded image: " + base64EncodedImage);
       
       
     }
