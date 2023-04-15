1a499c5736c9bc253051b9b80e4c328b58d80825
Endri Binaku <57039368+BinakuEndri@users.noreply.github.com>
2023-04-15 13:13:08 +0200

    Update Base64en_de_pic.java

--- Base64en_de_pic.java
+++ Base64en_de_pic.java
@@ -7,10 +7,15 @@ public class Picture {
         // Load the image into the bytes array
         byte[] imageBytes = Files.readAllBytes(Paths.get("C:\\Users\\ccc\\Pictures\\Saved Pictures\\Solid_black.jpg"));
         
-         // Encode the string of bytes
+        // Encode the string of bytes
         String base64EncodedImage = Base64.getEncoder().encodeToString(imageBytes);
         System.out.println("Encoded image: " + base64EncodedImage);
+        
+        // Decode the Base64 encoded string into a byte string
+        byte[] decodedImageBytes = Base64.getDecoder().decode(base64EncodedImage);
       
-      
+        // Write the string of bytes to a file on disk
+        Files.write(Paths.get("C:\Users\ccc\Desktop\image.bmp"), decodedImageBytes);
+        System.out.println("Decoded image saved to disk.");
     }
 }
